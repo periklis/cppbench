c3431c399ab163de5165324790f31236fb46003c
Periklis Tsirakidis <periklis.tsirakidis@mayflower.de>
2016-06-27 10:35:46 +0200

    Fix cmake project lang setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,5 @@ apt-add-repository -y ppa:ubuntu-toolchain-r/test
 apt-add-repository -y ppa:george-edison55/cmake-3.x
 apt-get update -qq
 apt-get install -yqq automake build-essential cmake gcc-5 g++-5 git doxygen nginx-full zsh
-apt-get install -yqq libboost1.55-all-dev
+# apt-get install -yqq libboost1.55-all-dev
 chsh -s $(which zsh)
